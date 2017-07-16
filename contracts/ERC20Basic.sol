4a78abfc19303207373367eff8887cb98da7cd09
ttata <umcoin@protonmail.com>
2017-07-16 13:49:16 -0700

    add basic community voting - up version

--- contracts/ERC20Basic.sol
+++ contracts/ERC20Basic.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.2;
 
 contract ERC20Basic {
   uint public totalSupply;
--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.2;
 
 library SafeMath {
   function mul(uint a, uint b) internal returns (uint) {
