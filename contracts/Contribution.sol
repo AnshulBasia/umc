3fa2b4d7edecb1eba67c133c3c9f35292ea67349
Terry <tataterry89@gmail.com>
2017-07-29 16:19:28 -0700

    fix compilation issues - tests will run

--- contracts/Contribution.sol
+++ contracts/Contribution.sol
@@ -21,7 +21,7 @@ contract Contribution is Pausable, PullPayment {
 	uint private constant MIN_CONTRIBUTE_ETHER = 100 finney;
 
     /* Max UmbrellaCoin cap of the Contribution*/
-    unit private constant MAX_CONTRIBUTE_ETHER = 2000 finney; //2000 UmbrellaCoin
+    uint private constant MAX_CONTRIBUTE_ETHER = 2000 finney; //2000 UmbrellaCoin
 
     /* Flat rate contribution multiplier*/
     uint private constant MAX_PAYOUT = 5; // 5x possible payout based on contrubution
@@ -30,13 +30,13 @@ contract Contribution is Pausable, PullPayment {
 	uint public constant COIN_PER_ETHER = 600000000; // 600 UmbrellaCoins
     
     /* Percentage of total float acsessable for Contribution*/
-    float private constant PERCENTAGE_OF_FLOAT = 0.01; //1%
+    uint private constant PERCENTAGE_OF_FLOAT = 100; //1%
 
 	/*
 	* Variables
 	*/
 	/* Map a starttime to an Umnbrella coin amount */
-	mapping(uint => UmbrellaCoin) private contributionHistory;
+	mapping(uint => uint) private contributionHistory;
 
 	/*
 	 * Event
@@ -47,13 +47,13 @@ contract Contribution is Pausable, PullPayment {
 /*Helper Functions*/
 
     /*Function to get current float*/
-    function CurrentFloat() private
+    function CurrentFloat() private returns (uint)
     {
         throw; //TODO: need to get current float
     }
 
 /*Function to convert finney to UmbrellaCoin*/
-    function FinneyToUmbrella(finney amount) private
+    function FinneyToUmbrella(uint amount) private returns (uint)
     {
         return amount.mul(COIN_PER_ETHER).div(1 ether);
     }
@@ -61,17 +61,17 @@ contract Contribution is Pausable, PullPayment {
 	/*
 	 * Constructor
 	*/
-    function Contribution(address _umbrellaCoinAddress, finney contributionAmount)
+    function Contribution(address _umbrellaCoinAddress, uint contributionAmount)
     {
-        if (contributionAmount < MIN_INVEST_ETHER) throw; // Don't accept too small of a policy
+        if (contributionAmount < MIN_CONTRIBUTE_ETHER) throw; // Don't accept too small of a policy
 
         if (contributionAmount >= MAX_CONTRIBUTE_ETHER) throw; //Don't aceept too large a policy
 
-        if (contributionAmount > CurrentFloat().mul(PERCENTAGE_OF_FLOAT)) throw; //Don't accept to large a policy
+        if (contributionAmount > CurrentFloat().div(PERCENTAGE_OF_FLOAT)) throw; //Don't accept to large a policy
 
         //TODO: Any other security checks here
 
-        contributionHistory.add(now, FinneyToUmbrella(contributionAmount));
+        contributionHistory[now] = FinneyToUmbrella(contributionAmount);
     }
 
 }
