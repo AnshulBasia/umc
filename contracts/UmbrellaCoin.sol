6f9d4513b140cc60c1ebaa054eff097f193d806b
Terry <tataterry89@gmail.com>
2017-08-05 17:10:58 -0700

    we should be ready to deploy

--- contracts/UmbrellaCoin.sol
+++ contracts/UmbrellaCoin.sol
@@ -11,23 +11,13 @@ contract UmbrellaCoin is StandardToken, Ownable {
   string public constant name = "UmbrellaCoin";
   string public constant symbol = "UMC";
   uint public constant decimals = 6;
-  address public constant floatHolder = 0x1A3C91B9Dfa069f5da7f24001777B161f5e0Fe60;
+  address public floatHolder;
 
   // Constructor
   function UmbrellaCoin() {
       totalSupply = 100000000000000;
       balances[msg.sender] = totalSupply; // Send all tokens to owner
-  }
-
-  /**
-   *  Burn away the specified amount of UmbrellaCoin tokens
-   */
-  function float(uint _value) onlyOwner returns (bool) {
-    require (_value >= 1); // don't allow invald values.
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    totalSupply = totalSupply.sub(_value);
-    Transfer(msg.sender, floatHolder, _value);
-    return true;
+      floatHolder = msg.sender;
   }
 
 }
--- truffle.js
+++ truffle.js
@@ -4,6 +4,22 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    live: {
+      network_id: 1, // Ethereum public network
+    // optional config values
+    // host - defaults to "localhost"
+    // port - defaults to 8545
+    // gas
+    // gasPrice
+    //  from  
+    },
+    rinkeby: {
+      network_id: 4,        // Official Ethereum test network
+      host: "localhost",
+      port: 8545,
+      gas: 21000,
+      from: "0x277423818E720A4F43Ad73F1C2fFFe2E649b64bE"             
     }
   }
 };
